6be5fed2faa315892d38f23471f76ecea09ae3e5
Gabriel Garrido <gabrielgarridoag@dcc.ufrj.br>
2021-02-08 22:22:00 -0300

    Primeiro commit. Inicializando o Spring.

---
+++ src/main/java/com/garr/ws/WsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.garr.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringApplication.class, args);
+	}
+
+}
