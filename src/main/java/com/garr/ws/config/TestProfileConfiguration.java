40f212ddf09f00772836b3214c97838a36271891
Gabriel Garrido <gabrielgarridoag@dcc.ufrj.br>
2021-02-13 00:53:02 -0300

    Implementando a classe Payment e associação one-to-one.

--- src/main/java/com/garr/ws/config/TestProfileConfiguration.java
+++ src/main/java/com/garr/ws/config/TestProfileConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.garr.ws.entities.Category;
 import com.garr.ws.entities.Order;
 import com.garr.ws.entities.OrderItem;
+import com.garr.ws.entities.Payment;
 import com.garr.ws.entities.Product;
 import com.garr.ws.entities.User;
 import com.garr.ws.entities.enums.OrderStatus;
@@ -75,6 +76,10 @@ public class TestProfileConfiguration implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 }
