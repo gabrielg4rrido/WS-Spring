d6305301ec96d567631459f2c15d5e71c822e47d
Gabriel Garrido <gabrielgarridoag@dcc.ufrj.br>
2021-02-13 01:01:31 -0300

    Implementando métodos Total e SubTotal.

--- src/main/java/com/garr/ws/entities/Order.java
+++ src/main/java/com/garr/ws/entities/Order.java
@@ -104,7 +104,17 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		
+		for (OrderItem orderItem : itens) {
+			total += orderItem.getSubTotal();
+		}
+		
+		return total;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/garr/ws/entities/Payment.java
+++ src/main/java/com/garr/ws/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="tb_payment")
 public class Payment implements Serializable {
@@ -22,6 +24,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
@@ -51,7 +54,7 @@ public class Payment implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-
+	
 	public Order getOrder() {
 		return order;
 	}
